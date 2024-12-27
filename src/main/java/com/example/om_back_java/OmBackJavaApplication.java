00c3bec7aed29d2ce4a3cecf2a416175f8c9a9ae
Natã Elias da Silva <nsilva@sistemasalutem.com.br>
2024-12-27 10:22:48 -0300

    feat: Add initial folders

---
+++ src/main/java/com/example/om_back_java/OmBackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.om_back_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmBackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmBackJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/om_back_java/OmBackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.om_back_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmBackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
