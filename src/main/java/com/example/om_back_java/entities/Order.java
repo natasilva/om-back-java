04af439ddad4244d86261022b3ffc0ffbdaa708b
natasilva <nata.software@hotmail.com>
2024-12-30 10:43:07 -0300

    fix: Add entity adjustment

--- src/main/java/com/example/om_back_java/entities/Order.java
+++ src/main/java/com/example/om_back_java/entities/Order.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Table(name = "order")
+@Table(name = "\"order\"")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
