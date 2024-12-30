7730f81bdcf9a5e3e9c13b160c6a47e4e75e8ac1
Natã Elias da Silva <nsilva@sistemasalutem.com.br>
2024-12-30 08:15:05 -0300

    feat: Add controllers

---
+++ src/main/java/com/example/om_back_java/controllers/BurgerController.java
@@ -0,0 +1,10 @@
+package com.example.om_back_java.controllers;
+
+import com.example.om_back_java.entities.Burger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("burgers")
+public class BurgerController {
+}
